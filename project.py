6248a922bdee32d9bda9f916351c5b8f331c54c5
Kirill554545 <166030822+Kirill554545@users.noreply.github.com>
2025-01-29 07:24:33 +0400

    Добавлены уровни в игре и кнопка "Главное меню" при прохождении всех уровней

--- project.py
+++ project.py
@@ -1,8 +1,11 @@
-import pygame
 import random
+
+import pygame
 
 # Инициализация Pygame
 pygame.init()
+
+level = 1
 
 # Устанавливаем размеры окна
 WIDTH, HEIGHT = 800, 600
@@ -14,7 +17,6 @@ zucchini_path = "data/zucchini.png"  # Путь к изображению для
 pear_path = "data/pear.png"  # Путь к изображению для груши
 apple_path = "data/apple.png"  # Путь к изображению для яблока
 player_path = "data/snake.png"  # Путь к изображению для игрока
-
 
 # Загружаем изображения
 zucchini_image = pygame.image.load(zucchini_path).convert_alpha()
@@ -34,7 +36,11 @@ pear_image = pygame.transform.scale(pear_image, (pear_size, pear_size))
 apple_image = pygame.transform.scale(apple_image, (apple_size, apple_size))
 player_image = pygame.transform.scale(player_image, (player_size, player_size))
 
-MAX_OBJECTS = 10  # Максимальное количество падающих объектов на экране
+MAX_OBJECTS = 10  # Определяем максимальное количество падающих объектов на экране
+
+button_rect = pygame.Rect(300, 300, 200, 100)  # x, y, ширина, высота
+fon = pygame.font.Font(None, 36)
+button_text = fon.render("Главное меню", True, (255, 255, 255))
 
 
 # Класс падающих объектов
@@ -51,8 +57,13 @@ class FallingObject:
         self.speed = [3, 2.5, 2][obj_type]  # Скорость падения
         self.obj_type = obj_type  # Сохраняем тип объекта
 
-    def fall(self):
-        self.y += self.speed
+    def fall(self, lvl):
+        if lvl == 1:
+            self.y += self.speed
+        elif lvl == 2:
+            self.y += self.speed * 1.5
+        elif lvl == 3:
+            self.y += self.speed * 2
 
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))  # Рисуем изображение
@@ -83,13 +94,14 @@ class Player:
 
 # Функция для перезапуска игры
 def reset_game():
-    return [], Player(), 0, False, False  # Возвращаем начальные значения
+    return [], Player(), 0, False, False, level  # Возвращаем начальные значения
 
 
 # Основной игровой цикл
 def main():
     clock = pygame.time.Clock()
-    falling_objects, player, score, game_over, player_won = reset_game()
+    global level
+    falling_objects, player, score, game_over, player_won, level = reset_game()
 
     running = True
     game_started = False  # Состояние начала игры
@@ -99,8 +111,23 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # Левую кнопку мыши
+                    if button_rect.collidepoint(
+                        event.pos
+                    ):  # Проверка, нажатие внутри кнопки
+                        game_started = False
+                        (
+                            falling_objects,
+                            player,
+                            score,
+                            game_over,
+                            player_won,
+                            level,
+                        ) = reset_game()
+
         if not game_started:
-            screen.fill((255, 255, 255))
+            screen.fill((255, 255, 255))  # Устанавливаем белый фон
             font = pygame.font.Font(None, 74)
             start_text = font.render("Нажмите Enter, чтобы начать", True, (0, 0, 0))
             text_rect = start_text.get_rect(center=(WIDTH // 2, HEIGHT // 3))
@@ -111,7 +138,7 @@ def main():
                 game_started = True
 
         else:
-            if not game_over and not player_won:
+            if not game_over and not player_won and level < 4:
                 keys = pygame.key.get_pressed()
                 if keys[pygame.K_a]:  # Движение влево
                     player.move(-player.speed)
@@ -125,25 +152,30 @@ def main():
 
                 # Обновляем позиции объектов и проверяем на столкновение с игроком
                 for obj in falling_objects:
-                    obj.fall()
+                    obj.fall(level)
+
                     # Проверяем на столкновение
-                    if player.get_rect().colliderect(pygame.Rect(obj.x, obj.y, obj.size[0], obj.size[1])):
+                    if player.get_rect().colliderect(
+                        pygame.Rect(obj.x, obj.y, obj.size[0], obj.size[1])
+                    ):
                         if obj.obj_type == 0:  # Большой объект
-                            score -= 2  # Уменьшаем счет на 2 очка
+                            score -= 2  # Уменьшаем на 2 очка
                         elif obj.obj_type == 1:  # Средний объект
-                            score += 2  # Увеличиваем счет на 2 очка
+                            score += 2
                         elif obj.obj_type == 2:  # Маленький объект
-                            score += 1  # Увеличиваем счет на 1 очко
+                            score += 1
+
                         # Удаляем объект после столкновения
                         falling_objects.remove(obj)
 
                 # Проверяем, не достиг ли счет отрицательных значений
-                if score < 0:
+                if score < 0 or level == 4:
                     game_over = True  # Устанавливаем флаг окончания игры
 
                 # Проверяем достижение счета 30 для победы
                 if score >= 30:
                     player_won = True  # Устанавливаем флаг победы
+                    level += 1
 
                 # Удаляем объекты, которые вышли за границы экрана
                 falling_objects = [obj for obj in falling_objects if obj.y < HEIGHT]
@@ -155,44 +187,82 @@ def main():
                 for obj in falling_objects:
                     obj.draw(screen)
 
-                # Рисуем игрока
+                # Рисуем управляемый объект
                 player.draw(screen)
 
                 # Отображаем счет
                 font = pygame.font.Font(None, 36)
                 score_text = font.render(f"Счет: {score}", True, (0, 0, 0))
-                screen.blit(score_text, (10, 10))
+                screen.blit(
+                    score_text, (10, 10)
+                )  # Размещение счета в верхнем левом углу
 
             else:
-                if game_over:
+                # Проверяем, проиграл игрок или победил
+                if game_over and level < 4:
                     # Отображение текста "Game Over!" с первоначальным размером шрифта
+
                     font = pygame.font.Font(None, 74)  # Оставляем размер шрифта 74
                     game_over_text = font.render("Game Over!", True, (255, 0, 0))
-                    text_rect = game_over_text.get_rect(center=(WIDTH // 2, HEIGHT // 3))
+                    text_rect = game_over_text.get_rect(
+                        center=(WIDTH // 2, HEIGHT // 3)
+                    )
                     screen.blit(game_over_text, text_rect)
-                    # Отображение текста "Нажмите пробел чтобы начать заново"
-                    smaller_font = pygame.font.Font(None, 36)  # Уменьшаем размер шрифта для второго текста
-                    restart_text = smaller_font.render("Нажмите пробел чтобы начать заново", True, (0, 0, 0))
-                    restart_rect = restart_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+                    # Отображение текста "Нажмите пробел чтобы начать заново" с уменьшенным размером шрифта
+                    smaller_font = pygame.font.Font(
+                        None, 36
+                    )  # Уменьшаем размер шрифта для второго текста
+                    restart_text = smaller_font.render(
+                        "Нажмите пробел чтобы начать заново", True, (0, 0, 0)
+                    )
+                    restart_rect = restart_text.get_rect(
+                        center=(WIDTH // 2, HEIGHT // 2)
+                    )
                     screen.blit(restart_text, restart_rect)
+                    level = 1
 
-                elif player_won:
-                    # Отображение текста "Вы победили!"
+                elif player_won or level == 4:
+                    # Отображение текста "Вы победили!" с первоначальным размером шрифта
                     font = pygame.font.Font(None, 74)  # Оставляем размер шрифта 74
-                    win_text = font.render("Вы победили!", True, (0, 255, 0))
+                    text = ""
+                    rst_text = ""
+                    if level == 2:
+                        text = "Вы прошли первый уровень!"
+                        rst_text = "Нажмите пробел чтобы запустить следующий уровень"
+                    elif level == 3:
+                        text = "Вы прошли второй уровень!"
+                        rst_text = "Нажмите пробел чтобы запустить следующий уровень"
+                    elif level == 4:
+                        text = "Вы победили!"
+                        level = 1
+                        pygame.draw.rect(screen, (0, 0, 255), button_rect)
+                        screen.blit(
+                            button_text, (button_rect.x + 20, button_rect.y + 30)
+                        )
+                    win_text = font.render(text, True, (0, 255, 0))
                     text_rect = win_text.get_rect(center=(WIDTH // 2, HEIGHT // 3))
                     screen.blit(win_text, text_rect)
-                    # Отображение текста "Нажмите пробел чтобы начать заново" с уменьшенным размером шрифта
-                    smaller_font = pygame.font.Font(None, 36)
-                    restart_text = smaller_font.render("Нажмите пробел чтобы начать заново", True, (0, 0, 0))
-                    restart_rect = restart_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+
+                    # Отображение текста "Нажмите пробел чтобы начать заново"
+                    smaller_font = pygame.font.Font(
+                        None, 36
+                    )  # Уменьшаем размер шрифта для второго текста
+                    restart_text = smaller_font.render(rst_text, True, (0, 0, 0))
+                    restart_rect = restart_text.get_rect(
+                        center=(WIDTH // 2, HEIGHT // 2)
+                    )
                     screen.blit(restart_text, restart_rect)
                     # Проверяем нажатие пробела для перезапуска игры
                 keys = pygame.key.get_pressed()
                 if keys[pygame.K_SPACE]:
-                    falling_objects, player, score, game_over, player_won = reset_game()  # Перезапуск игры
-
-                # Обновляем экран
+                    (
+                        falling_objects,
+                        player,
+                        score,
+                        game_over,
+                        player_won,
+                        level,
+                    ) = reset_game()
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
